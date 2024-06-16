11e362e789ae792d518841daba38ee823722b9da
Patrick Taburet <taburet.patrick@gmail.com>
2024-06-16 23:22:27 +0200

    Responsive and front adjusments

---
+++ public/js/myArtworks.js
@@ -0,0 +1,10 @@
+// Lightbox config :
+
+document.addEventListener('DOMContentLoaded', function () {
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': false,
+        'showImageNumberLabel': true,
+        'alwaysShowNavOnTouchDevices': true
+    });
+});
