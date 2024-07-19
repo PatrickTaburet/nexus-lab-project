6d3a2a67e1ec12b7d8e1de214d6e7c6037e675e0
Patrick Taburet <taburet.patrick@gmail.com>
2024-07-20 00:32:34 +0200

    Refs optimisation and twig refactoring.

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -2,19 +2,25 @@
 
 namespace App\Security;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Security;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\HttpFoundation\{
+    Response,
+    Request,
+    RedirectResponse
+};
+use Symfony\Component\Security\{
+    Core\Security,
+    Core\Authentication\Token\TokenInterface
+};
+use Symfony\Component\Security\Http\{
+    Authenticator\Passport\Badge\UserBadge,
+    Authenticator\Passport\Badge\CsrfTokenBadge,
+    Authenticator\Passport\Badge\RememberMeBadge,
+    Authenticator\AbstractLoginFormAuthenticator,
+    Authenticator\Passport\Credentials\PasswordCredentials,
+    Authenticator\Passport\Passport,
+    Util\TargetPathTrait,
+};
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 
 
 class LoginAuthenticator extends AbstractLoginFormAuthenticator
