7784b8ce845169df52459330a8788691b7b292d8
Patrick Taburet <taburet.patrick@gmail.com>
2024-09-08 14:48:57 +0200

    Front ProfileScreen ok.

--- mobile/nexuslab_mobile/src/screen/ProfileScreen.jsx
+++ mobile/nexuslab_mobile/src/screen/ProfileScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, StyleSheet, TouchableOpacity, SafeAreaView, Image, Animated, Easing } from 'react-native';
+import { ImageBackground, View, Text, Button, StyleSheet, TouchableOpacity, SafeAreaView, Image, Animated, Easing } from 'react-native';
 import React, { useState, useEffect } from 'react'
 import { CommonActions, useIsFocused } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -9,6 +9,7 @@ import config from '../config/config';
 import { colors } from '../utils/colors'
 import MyButton from '../components/MyButton';
 import globalStyles from '../utils/styles';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 const ProfileScreen = ({ navigation, setIsLoggedIn })  => {
 
@@ -89,13 +90,35 @@ const ProfileScreen = ({ navigation, setIsLoggedIn })  => {
     outputRange: ['0deg', '360deg'],
   });
 
+  const getContainerHeight = () => {
+    if (!userData) return '83%'; 
+
+    let height = '70%'; // Hauteur minimale par défaut
+
+    if (userData.roles[1] || userData.roles[2]) {
+      height = '75%'; // Augmentez la hauteur si un des rôles est présent
+    }
+    if (userData.roles[1] && userData.roles[2]) {
+      height = '83%'; // Hauteur maximale si les deux rôles sont présents
+    }
+
+    return height;
+  };
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.closeButton}>
-          <Ionicons name="close" size={38} color="black" />
-        </TouchableOpacity>
-      </View>
+    <ImageBackground
+    source={require('../assets/design/backgroundProfile.jpg')}
+    style={styles.backgroundImage}
+    resizeMode="cover"
+  >
+    <View style={styles.header}>
+      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.closeButton}>
+        <Ionicons name="close" size={38} color="white" />
+      </TouchableOpacity>
+    </View>
+    <SafeAreaView style={[styles.globalContainer, { height: getContainerHeight() }]}>
+      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.editButton}>
+        <FontAwesome5 name="user-edit" size={24} color="white" />
+      </TouchableOpacity>
       <View style={styles.container}>
          {userData ? (
           <>
@@ -113,8 +136,8 @@ const ProfileScreen = ({ navigation, setIsLoggedIn })  => {
             />
             <Text style={styles.text}>Pseudo : {userData.pseudo}</Text>
             <Text style={styles.text}>Email: {userData.email}</Text>
-            <Text style={styles.text}>Role: {userData.roles}</Text>
-            {/* Ajoutez d'autres champs utilisateur ici */}
+            <Text style={styles.text}>Role : {userData.roles[0] ? "USER" : ""}{userData.roles[1] ? " / ARTIST" : ""}{userData.roles[2] ? " / ADMIN" : ""}</Text>
+         
           </>
         ) : (
           <Text style={styles.text}>Aucune donnée utilisateur disponible</Text>
@@ -134,75 +157,103 @@ const ProfileScreen = ({ navigation, setIsLoggedIn })  => {
               My Artwork
             </MyButton>
           </View>
+          {userData.roles[1] && (
+            <MyButton
+              // HandlePress={}
+              myStyle={styles.secondButton}
+              isSecondary={true}
+            >
+              Artist Dashboard
+            </MyButton>
+          )}
+        </View>
+        {userData.roles[2] && (
           <MyButton
             // HandlePress={}
             myStyle={styles.secondButton}
             isSecondary={true} 
           >
-            Artist Dashboard
+            Admin Dashboard
           </MyButton>
-        </View>
-       
-        <MyButton
-          // HandlePress={}
-          myStyle={styles.secondButton}
-          isSecondary={true} 
-        >
-          Admin Dashboard
-        </MyButton>
+        )}
         <TouchableOpacity onPress={handleLogout}>
           <Text style={[styles.logoutText, globalStyles.text3]}>Logout</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
+    </ImageBackground>
+
   );
 }
 
 export default ProfileScreen
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-
+  backgroundImage: {
+    flex: 1,   
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  safeArea:{
+    flex: 1
+  },
+  globalContainer : {
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    width: '80%',
+    height: '83%',
+    borderRadius: 20,
+    borderColor: colors.cyan,
+    borderWidth: 2,
+    marginTop: 30
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
+    width:'100%',
+    position: 'absolute',
+    top: 47
   },
   closeButton: {
-    marginRight: 23,
-    marginTop: 35
+    position: 'absolute',
+    right: 12,
+    top: -10,
+  },
+  editButton:{
+    width: 'auto',
+    display:"flex",
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    padding: 12
   },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 50,
-    gap: 15
+    gap: 12,
+    paddingBottom: 150,
   },
   text: {
-    fontSize: 20,
-    marginBottom: 20,
+    fontSize: 15,
+    marginBottom: 10,
+    color: 'white'
   },
   logoutText:{
     fontSize: 23,
-    color: colors.primary
+    color: colors.primary,
+    marginTop: 5,
+    marginBottom: 15
   },
   profileImage: {
-    width: 127,
-    height: 127,
+    width: 128,
+    height: 128,
     borderRadius: 70,
     zIndex:2,
-    marginBottom: 10,
-    marginTop: 50
+    position: "relative",
+    top : 161,
+
   },
   circleImg:{
     width: 170,
     height: 170,
-    position: "absolute",
-    top : 15,
-    marginBottom: 30
+    position: "relative",
   },
   secondButton:{
     width: 150,
@@ -222,5 +273,6 @@ const styles = StyleSheet.create({
   globalButtonBox:{
     display:"flex",
     alignItems:'center',
-  }
+  },
+
 })
